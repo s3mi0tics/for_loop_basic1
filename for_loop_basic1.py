973d8d623b76ee3afd826e04d82240984f98db5e
Colby Kauk <colby.kauk@gmail.com>
2021-11-29 18:07:44 -0700

    Initial commit

---
+++ for_loop_basic1.py
@@ -0,0 +1,32 @@
+for i in range(151):
+    print(i)
+
+
+
+for j in range(5, 1005, 5):
+    print(j)
+
+
+
+for Dojo_Counting in range(1,101):
+    if Dojo_Counting%10==0:
+        print("Coding Dojo")
+    elif Dojo_Counting%5==0:
+        print("Coding")
+    else:
+        print(Dojo_Counting)
+
+Woah= 0
+for add_odd in range(5000001):
+    if add_odd%2!=0:
+        Woah += add_odd
+print(Woah)
+
+for down4 in range(2018,0,-4):
+    print(down4)
+
+low_num = 4
+high_num = 65
+mult = 7
+for flex_count in range(low_num, high_num, mult):
+    print(flex_count)
